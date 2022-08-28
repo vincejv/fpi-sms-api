22a5aa992475fe747d90703299b7fc6a47c7d679
Vince Jerald Villamora <vincevillamora@gmail.com>
2022-08-28 20:43:02 +0800

    Missing file

---
+++ src/main/java/com/abavilla/fpi/dto/impl/api/load/RewardsReqDto.java
@@ -0,0 +1,35 @@
+package com.abavilla.fpi.dto.impl.api.load;
+
+import com.abavilla.fpi.dto.AbsDto;
+import com.abavilla.fpi.dto.AbsFieldDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@RegisterForReflection
+public class RewardsReqDto extends AbsDto {
+
+  @JsonProperty("outboundRewardRequest")
+  private Body body;
+
+  @Data
+  @EqualsAndHashCode(callSuper = true)
+  @NoArgsConstructor
+  @RegisterForReflection
+  public static class Body extends AbsFieldDto {
+    @JsonProperty("app_id")
+    private String appId;
+    @JsonProperty("app_secret")
+    private String appSecret;
+    @JsonProperty("rewards_token")
+    private String rewardsToken;
+    private String address;
+    @JsonProperty("promo")
+    private String sku;
+  }
+}
